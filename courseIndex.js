fd14f41669594ac63ed2250b6bc50f54c48b1266
AdamTefik <102264539+AdamTefik@users.noreply.github.com>
2022-03-23 18:47:25 -0400

    Add files via upload

---
+++ courseIndex.js
@@ -0,0 +1,74 @@
+var bed;
+var laptop;
+var chair;
+var dresser = ["shirts", "pants", "coats"];
+var carpet;
+var walls;
+var blanket = {colour: "blue", pattern: "checkered"};
+var poster = (5);
+var shelves;
+var desk = "wooden";
+
+// bed.unmade = true;
+
+
+
+
+var x = 12
+var y = 6
+
+
+sleep(x,y)
+
+
+
+function sleep(timeWentToSleep, alarmTimeSet){
+    let t = x;
+    let a = y;
+    var s = t - a;
+    let b = Math.floor(s);
+    if(s > 12){
+        s - 12
+        console.log(b);
+    }else
+    console.log(b);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Course Index: First Page");
